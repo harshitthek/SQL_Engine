6ada9d0e1f749f826ad57368564ec223d5a97b26
here-2007 <pushkarj401@gmail.com>
2026-09-06 17:25:15 +0530

    refactor(evaluation): export evaluator symbols in src.evaluation.__init__

---
+++ src/evaluation/__init__.py
@@ -0,0 +1,31 @@
+"""Evaluation package for Text-to-SQL."""
+
+from .evaluator import (
+    EvalItemResult,
+    ExecutionResult,
+    SQLEvaluator,
+    SandboxViolationError,
+    compare_result_sets,
+    compute_exact_match,
+    execute_query,
+    get_db_path,
+    normalize_for_em,
+    normalize_sql,
+    sort_select_columns,
+    validate_sandbox,
+)
+
+__all__ = [
+    "EvalItemResult",
+    "ExecutionResult",
+    "SQLEvaluator",
+    "SandboxViolationError",
+    "compare_result_sets",
+    "compute_exact_match",
+    "execute_query",
+    "get_db_path",
+    "normalize_for_em",
+    "normalize_sql",
+    "sort_select_columns",
+    "validate_sandbox",
+]
